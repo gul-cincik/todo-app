9b0de8dd91df648a3ce6fdede48139b9c5a35ffc
gul-cincik <48259821+gul-cincik@users.noreply.github.com>
2023-11-01 14:36:38 +0300

    s

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+const EditTodoForm = ({editTodo, task}) => {
 
-function EditTodoForm() {
+    const [value, setValue] = useState(task.task);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        editTodo(value, task.id);
+
+        setValue("");
+    }
   return (
-    <div>EditTodoForm</div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type ='text' className='todo-input' value={value}  placeholder='Görevi Güncelle' onChange={(e) => setValue(e.target.value)}/>
+        <button type='submit'className='todo-btn'>Güncelle</button>
+    </form>
   )
 }
 
 export default EditTodoForm;
+
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,12 +5,12 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 //rafc
 
-const Todo = ({task, toggleComplete, deleteTodo}) => {
+const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
   return (
     <div className='Todo'>
         <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? "completed": ""}`}>{task.task}</p>
         <div>
-            <FontAwesomeIcon className='edit-icon' icon={faPenToSquare} />
+            <FontAwesomeIcon className='edit-icon' icon={faPenToSquare} onClick={() => editTodo(task.id)} />
             <FontAwesomeIcon className='delete-icon' icon = {faTrash} onClick={() => deleteTodo(task.id)}/>
 
         </div>
